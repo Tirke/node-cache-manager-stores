f3667b9229f6c470b22433532f989316107984e2
Tirke <tirke@tirke.dev>
2022-08-09 08:59:24 +0200

    chore: add codecov

--- jest.preset.js
+++ jest.preset.js
@@ -1,10 +1,7 @@
 module.exports = {
   testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
   resolver: '@nrwl/jest/plugins/resolver',
+  coverageReporters: ['text', 'cobertura'],
   moduleFileExtensions: ['ts', 'js', 'mjs', 'html'],
-  coverageReporters: ['html'],
-  transform: {
-    '^.+\\.[tj]s$': '@swc/jest',
-  },
   testEnvironment: 'node',
 };
