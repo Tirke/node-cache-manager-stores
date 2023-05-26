c09821da87e59d538fa0992ea20332238fa3dee0
Tirke <tirke@tirke.dev>
2023-05-26 15:40:38 +0200

    feat: bump nx

--- jest.preset.js
+++ jest.preset.js
@@ -1,6 +1,6 @@
 module.exports = {
   testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-  resolver: '@nrwl/jest/plugins/resolver',
+  resolver: '@nx/jest/plugins/resolver',
   coverageReporters: ['text', 'cobertura', 'html'],
   moduleFileExtensions: ['ts', 'js', 'mjs', 'html'],
   testEnvironment: 'node',
